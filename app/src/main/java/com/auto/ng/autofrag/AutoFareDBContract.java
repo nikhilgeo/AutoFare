1c332ae64f96d57b86b1550a88e6e7f820a97c24
Nikhil George <nikhilgeorge@vmware.com>
2015-12-28 18:17:38 +0530

    Sqlite table template worked out

---
+++ app/src/main/java/com/auto/ng/autofrag/AutoFareDBContract.java
@@ -0,0 +1,25 @@
+package com.auto.ng.autofrag;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by nikhilgeorge on 28-Dec-15.
+ */
+public class AutoFareDBContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public AutoFareDBContract() {
+    }
+
+    /* Inner class that defines the table contents */
+    public static abstract class BaseFareTemplate implements BaseColumns {
+        public static final String TABLE_NAME = "BaseFare";
+        public static final String COLUMN_NAME_minCharge = "minCharge";
+        public static final String COLUMN_NAME_minCharge_KM = "minCharge_KM";
+        public static final String COLUMN_NAME_additionalFare = "additionalFare";
+        public static final String COLUMN_NAME_additionalFare_KM = "additionalFare_KM";
+        public static final String COLUMN_NAME_nightCharge = "nightCharge";
+        public static final String COLUMN_NAME_waitingCharge = "waitingCharge";
+        public static final String COLUMN_NAME_waitingCharge_Min = "waitingCharge_Min";
+    }
+}
