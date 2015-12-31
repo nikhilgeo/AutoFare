76def619f0b38fecbdb73f08cd422ea6b758a777
nikhilgeo <nikhilgeo@gmail.com>
2015-12-31 13:39:56 +0530

    App Indexing API removed

--- app/src/main/java/com/auto/ng/autofrag/MainActivity.java
+++ app/src/main/java/com/auto/ng/autofrag/MainActivity.java
@@ -7,73 +7,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 import com.auto.ng.autofrag.AutoFareDBContract.BaseFareTemplate;
 
 public class MainActivity extends AppCompatActivity {
 
     String minCharge, minCharge_KM, additionalFare, additionalFare_KM,
             nightCharge, waitingCharge, waitingCharge_Min;
-
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
         getFare();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.auto.ng.autofrag/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.auto.ng.autofrag/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 
     private void getFare() {
         try {
@@ -102,13 +48,13 @@ public class MainActivity extends AppCompatActivity {
                 waitingCharge_Min = cursor.getString(cursor.getColumnIndexOrThrow(BaseFareTemplate.COLUMN_NAME_waitingCharge_Min));
 
                 TextView txtviewMinCharge = (TextView) findViewById(R.id.txtviewMinCharge);
-                txtviewMinCharge.setText(getResources().getString(R.string.Rs) + minCharge + " for " + minCharge_KM + "KM");
+                txtviewMinCharge.setText(getResources().getString(R.string.Rs) + minCharge + "/" + minCharge_KM + "KM");
                 TextView txtviewAddnCharge = (TextView) findViewById(R.id.txtviewAddnCharge);
-                txtviewAddnCharge.setText(getResources().getString(R.string.Rs) + additionalFare + " for " + additionalFare_KM + "KM");
+                txtviewAddnCharge.setText(getResources().getString(R.string.Rs) + additionalFare + "/" + additionalFare_KM + "KM");
                 TextView txtviewNightCharge = (TextView) findViewById(R.id.txtviewNightCharge);
                 txtviewNightCharge.setText(nightCharge + "% Extra");
                 TextView txtviewWatingCharge = (TextView) findViewById(R.id.txtviewWatingCharge);
-                txtviewWatingCharge.setText(getResources().getString(R.string.Rs) + waitingCharge + " for " + waitingCharge_Min + "Mins");
+                txtviewWatingCharge.setText(getResources().getString(R.string.Rs) + waitingCharge + "/" + waitingCharge_Min + "Mins");
             }
         } catch (Exception ex) {
             Log.e("AutoFare", ex.toString());
