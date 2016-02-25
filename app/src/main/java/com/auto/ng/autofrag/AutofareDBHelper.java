40ff9dbe0fda0d3fed12ef19d64ffabc6220e23a
Nikhil George <nikhilgeo@gmail.com>
2016-02-25 22:24:23 +0530

    removed the places table creation code

--- app/src/main/java/com/auto/ng/autofrag/AutofareDBHelper.java
+++ app/src/main/java/com/auto/ng/autofrag/AutofareDBHelper.java
@@ -6,8 +6,6 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import com.auto.ng.autofrag.AutoFareDBContract.BaseFareTemplate;
-import com.auto.ng.autofrag.AutoFareDBContract.StateCityTemplate;
-
 
 /**
  * Created by nikhilgeorge on 28-Dec-15.
@@ -16,7 +14,7 @@ public class AutofareDBHelper extends SQLiteOpenHelper {
 
     // If you change the database schema, you must increment the database
     // version.
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "autoFare.db";
     private static final String TEXT_TYPE = " TEXT";
     private static final String COMMA_SEP = ",";
@@ -33,12 +31,6 @@ public class AutofareDBHelper extends SQLiteOpenHelper {
             BaseFareTemplate.COLUMN_NAME_stateName + TEXT_TYPE +
             " )";
 
-    private static final String SQL_CREATE_ENTRIES_StateCity = "CREATE TABLE "
-            + StateCityTemplate.TABLE_NAME + " (" + StateCityTemplate._ID
-            + "AUTOINCREMENT INTEGER PRIMARY KEY" + COMMA_SEP +
-            StateCityTemplate.COLUMN_NAME_Place + TEXT_TYPE +
-            " )";
-
     private static final String SQL_INSERT_ENTRIES_BaseFare_Kerala = "INSERT INTO "
             + BaseFareTemplate.TABLE_NAME + "("
             + BaseFareTemplate.COLUMN_NAME_minCharge + COMMA_SEP
@@ -51,15 +43,8 @@ public class AutofareDBHelper extends SQLiteOpenHelper {
             + BaseFareTemplate.COLUMN_NAME_stateName + ")"
             + " VALUES ('20','1.5','10','1','50','10','15','Kerala')";
 
-    private static final String SQL_INSERT_ENTRIES_StateCity = "INSERT INTO "
-            + StateCityTemplate.TABLE_NAME + "("
-            + StateCityTemplate.COLUMN_NAME_Place + ")"
-            + " VALUES ("; //The places will be concated from the Utilities.Places List
-
     private static final String SQL_DELETE_ENTRIES_BaseFare = "DROP TABLE IF EXISTS "
             + BaseFareTemplate.TABLE_NAME;
-    private static final String SQL_DELETE_ENTRIES_StateCity = "DROP TABLE IF EXISTS "
-            + StateCityTemplate.TABLE_NAME;
 
     public AutofareDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -71,20 +56,11 @@ public class AutofareDBHelper extends SQLiteOpenHelper {
         try {
             Log.w("AutoFare:", SQL_CREATE_ENTRIES_BaseFare);
             db.execSQL(SQL_CREATE_ENTRIES_BaseFare);
-            Log.w("AutoFare:", SQL_CREATE_ENTRIES_StateCity);
-            db.execSQL(SQL_CREATE_ENTRIES_StateCity);
-
             Log.w("AutoFare:", SQL_INSERT_ENTRIES_BaseFare_Kerala);
             db.execSQL(SQL_INSERT_ENTRIES_BaseFare_Kerala);
 
-            for (String place : Utilities.places) {
-                Log.w("AutoFare:", SQL_INSERT_ENTRIES_StateCity + "'" + place + "')");
-                db.execSQL(SQL_INSERT_ENTRIES_StateCity + "'" + place + "')");
-            }
-
-
         } catch (Exception ex) {
-            Log.e("AutoFare", ex.toString());
+            //Log.e("AutoFare", ex.toString());
         }
     }
 
@@ -93,7 +69,6 @@ public class AutofareDBHelper extends SQLiteOpenHelper {
         // This database is only a cache for online data, so its upgrade policy
         // is to simply to discard the data and start over
         db.execSQL(SQL_DELETE_ENTRIES_BaseFare);
-        db.execSQL(SQL_DELETE_ENTRIES_StateCity);
         onCreate(db);
     }
 
